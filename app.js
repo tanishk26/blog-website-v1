0c3465a531c5766990b7a88d4395f2300c7cfd60
tanishk26 <tanishkthilakan01@gmail.com>
2021-09-11 11:26:23 +0530

    Hosted site on heroku

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const mongoose = require("mongoose");
 const _ = require('lodash');
 require("dotenv").config();
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to my Blog Website. Compose a blog if you like by adding /compose in the URL. You can also check out these blogs. Click on read more on any of the blogs which will redirect you to the entire blog. Click on contact for any queries and on about us to know more about this website. Enjoy your time on the site.";
 
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const aboutContent = "This site is made by Tanishk Thilakan as a side project to further his knowledge in Node.JS, Express.JS, MongoDB and EJS. The site is hosted on heroku and the database is connected via mongodb atlas. You can see the entire code on github profile named tanishk26";
 
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const contactContent = "Currently pursuing an undergrad degree in Computer and Communication Engineering at Manipal Institute of Technology. Prolific in C++ and acquiring more knowledge in MERN stack with each passing day. Contact me through mail which I have posted on my github profile named tanishk26";
 
 const app = express();
 
@@ -77,7 +77,10 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
 });
 
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == NULL || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server started successfully");
 });
